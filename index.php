cfbb710433d8452a767a230f2c29b4f9f288b4c5
rakaaist <rakaaist@gmail.com>
2020-11-06 10:46:25 +0200

    Add shop description and style

--- index.php
+++ index.php
@@ -15,10 +15,18 @@
         <ul class="nav">
             <li><img class="home-logo" src="https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/09/Erotic-Logo.jpg" alt="home"><a class="nav-links" href=""></a></li>
             <li><a class="nav-links" href="index.php">Katalogas</a></li>
-            <li><a class="nav-links" href="">Nustebink mane</a></li>
-            <li><a class="nav-links" href="">Nusiskundimai</a></li>
+            <li><a class="nav-links" href="surprise.php">Nustebink mane</a></li>
+            <li><a class="nav-links" href="nusiskundimai.php">Nusiskundimai</a></li>
         </ul>
     </nav>
 </header>
+<main class="home-main">
+    <section class="home-description">
+        <h4 class="home-info home-h4">SEKSO IR EROTINIŲ PREKIŲ PARDUOTUVĖ</h4>
+        <p class="home-info home-p">"Meilės asorti" - inovatoriškus meilės malonumus siūlanti parduotuvė. Gyvenimą mylinčios poros, moterys ir vyrai čia lengvai ir patogiai atranda viską, ko reikia aukštosioms akimirkoms pasiekti. Sekso prekių parduotuvėje rasite klasikinius ar novatoriškus vibratorius, masturbatorius, dildo solo ir poros intymiems žaidimams. Išmanieji sekso žaislai, žengiantys koja kojon kartu su technologijų pažanga, vaginaliniai kamuoliukai moterims, besirūpinančioms intymia sveikata ir seksualinio gyvenimo kokybe. Aukštos kokybės lubrikantas ir intymi kosmetika suteiks komfortą tiek solo, tiek poros žaidimuose.</p>
+        <h4 class="home-info home-h4">EROTINĖS PREKĖS JŪSŲ MALONUMUI</h4>
+        <P class="home-info home-p">Mes siekiame sukurti geriausią įmanomą apsipirkimo internetu patirtį. Dėmesį skiriame kiekvienai prekių paieškos, atrankos ir pirkimo proceso detalei. Siekiame, kad mūsų asortimente esančių prekių pasiūla atitiktų kiekvieno poreikius.</P>
+    </section>
+</main>
 </body>
 </html>
