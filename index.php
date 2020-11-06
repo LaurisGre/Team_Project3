6da101e282acf8807500c2fb03e2a5f6baa73870
rakaaist <rakaaist@gmail.com>
2020-11-06 10:12:11 +0200

    Add navigation

--- index.php
+++ index.php
@@ -6,10 +6,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style">
-    <title>Index</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Meilės asorti</title>
 </head>
 <body>
-    
+<header class="header">
+    <nav class="nav-wrapper">
+        <ul class="nav">
+            <li><img class="home-logo" src="https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/09/Erotic-Logo.jpg" alt="home"><a class="nav-links" href=""></a></li>
+            <li><a class="nav-links" href="index.php">Katalogas</a></li>
+            <li><a class="nav-links" href="">Nustebink mane</a></li>
+            <li><a class="nav-links" href="">Nusiskundimai</a></li>
+        </ul>
+    </nav>
+</header>
 </body>
 </html>
